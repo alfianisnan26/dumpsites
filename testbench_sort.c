649b1a04d487a4adce3409ecc1fded17497d8a5f
alfianisnan26 <49936990+alfianisnan26@users.noreply.github.com>
2020-03-24 06:53:30 +0700

    Update TB

--- testbench_sort.c
+++ testbench_sort.c
@@ -8,6 +8,7 @@
 
 float testbench(int, int, int, int);
 int* counting_sort(int*, int, int);
+void mergeSort(int arr[], int l, int r);
 
 int main(void) {
 	int show;
@@ -19,8 +20,8 @@ int main(void) {
 		show = _getch();
 		if (show == 'Y' || show == 'y') show = 1;
 		else show = 0;
-
-		for (int i = 0; i < 5; i++) {
+		int i;
+		for (i = 0; i < 5; i++) {
 			printf("Try %d | Data 10^%d | RT : %6.1f ns\n", try, i + 2, testbench(1, 100, pow(10, i + 2), show));
 			if (show == 1) system("pause");
 		}
@@ -39,7 +40,8 @@ float testbench(int min_number, int max_number, int n_data, int show) {
 
 	//Generating Array
 	int* arr = malloc(sizeof(int) * n_data);
-	for (int a = 0; a < n_data; a++) {
+	int a;
+	for (a = 0; a < n_data; a++) {
 		//Generating Random Number
 		arr[a] = rand() % (max_number + 1 - min_number) + min_number;
 	}
@@ -50,7 +52,8 @@ float testbench(int min_number, int max_number, int n_data, int show) {
 	clock_t rt = clock();
 	//Sorting
 
-	arr = counting_sort(arr, max_number, n_data); //SORTING FUNCTION HERE
+	//arr = counting_sort(arr, max_number, n_data);
+	mergeSort(arr, 0, n_data-1); //SORTING FUNCTION HERE
 
 	//Stop RunningTime Counter
 	rt = clock() - rt;
@@ -90,4 +93,75 @@ int* counting_sort(int* A, int k, int n)
 	free(A);
 	free(C);
 	return B;
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* masukkan array */
+	int* L = (int*)malloc(sizeof(int) * n1);
+	int* R = (int*)malloc(sizeof(int) * n2);
+	/* pindahkan data array L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	/* gabungkan kembali temp array ke arr arr[l..r]*/
+	i = 0; // subarray indeks pertama
+	j = 0; // subarray indeks kedua
+	k = l; // subarray indeks merged
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* pindahkan elemen yang tersisa dariL[],
+*/
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* pindahkan sisa elemen dari R[]
+*/
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+/* l adalah untuk indeks kiri dan r adalah indeks kanan
+	sub-array arr akan diurutkan
+*/
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		//         large l and h
+		int m = l + (r - l) / 2;
+
+		// urutkan bagian pertama dan kedua
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
+	}
 }
